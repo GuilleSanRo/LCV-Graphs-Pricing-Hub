da845da5ecd5976b4442f6522c128da73790989b
GuilleSanRo <willy956@gmail.com>
2026-06-10 11:06:56 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,15 @@
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 
+if (process.env.VERCEL) {
+  process.env.NITRO_PRESET = 'vercel';
+}
+
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  nitro: true,
   tanstackStart: {
-    ssr: false
+    server: { entry: "server" },
   },
 });

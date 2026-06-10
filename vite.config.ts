482899c65dd26fb99f331bc56adaf9bdbb1f7e52
GuilleSanRo <willy956@gmail.com>
2026-06-10 10:35:05 +0200

    Antigravity to be able to publish in Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 
-if (process.env.VERCEL) {
-  process.env.NITRO_PRESET = 'vercel';
-}
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
